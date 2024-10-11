2bcdb211710b54e5db6f0b13d63aad29e8e0ab7c
pblinkprosper <pblinkprosper01@gmail.com>
2024-10-11 14:42:15 +0000

    Added directory and a Readme file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Type-annoted function
+"""
+
+
+def add(num1: float, num2: float) -> float:
+    """function that adds two floats and returns their output"""
+    return (num1 + num2)
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is the Repository for the ALX backend python projects
